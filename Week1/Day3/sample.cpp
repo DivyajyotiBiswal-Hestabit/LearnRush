dbe1495900715118259c36202c11ad43a8ec528b
DivyajyotiBiswal-Hestabit <divyajyotib.hestabit@gmail.com>
2026-02-04 17:27:50 +0530

    Added a function

--- Week1/Day3/sample.cpp
+++ Week1/Day3/sample.cpp
@@ -1,10 +1,14 @@
 #include<iostream>
 using namespace std;
+int funcADD(int a,int b){
+    return a+b;
+}
 int main(){
     cout<<"Hii Hestabit"<<endl;
     cout<<"My name is Divyajyoti Biswal"<<endl;
     cout<<6/4<<endl;
     cout<<"Life is beautiful"
+    cout<<funcADD(6,7)<<endl;
     
     cout<<3+4<<endl;
     cout<<8*8;<<endl;
