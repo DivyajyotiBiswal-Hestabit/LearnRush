1917dd4a446d1a28c91f62fd0afce276ecf328a7
DivyajyotiBiswal-Hestabit <divyajyotib.hestabit@gmail.com>
2026-02-04 17:16:32 +0530

    Updated my name

--- Week1/Day3/sample.cpp
+++ Week1/Day3/sample.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     cout<<"Hii Hestabit"<<endl;
-    cout<<"My name is Divyajyoti"<<endl;
+    cout<<"My name is Divyajyoti Biswal"<<endl;
     cout<<"Life is beautiful";
     cout<<3+4<<endl;
     cout<<8*8<<endl;
