11cf99bd354f470d3b7c6f8502ebc17ceb3d0837
DivyajyotiBiswal-Hestabit <divyajyotib.hestabit@gmail.com>
2026-02-04 17:13:14 +0530

    Added math sum

--- Week1/Day3/sample.cpp
+++ Week1/Day3/sample.cpp
@@ -2,5 +2,7 @@
 using namespace std;
 int main(){
     cout<<"Hii Hestabit"<<endl;
-    cout<<"My name is Divyajyoti Biswal";
+    cout<<"My name is Divyajyoti"<<endl;
+    cout<<"Life is beautiful";
+    cout<<3+4<<endl;
 }
