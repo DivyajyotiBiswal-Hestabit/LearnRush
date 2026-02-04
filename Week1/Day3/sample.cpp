47f19b90d935bf456fe189242e3a444d4de35e29
DivyajyotiBiswal-Hestabit <divyajyotib.hestabit@gmail.com>
2026-02-04 17:26:06 +0530

    Another problem

--- Week1/Day3/sample.cpp
+++ Week1/Day3/sample.cpp
@@ -3,7 +3,9 @@ using namespace std;
 int main(){
     cout<<"Hii Hestabit"<<endl;
     cout<<"My name is Divyajyoti Biswal"<<endl;
+    cout<<6/4<<endl;
     cout<<"Life is beautiful"
+    
     cout<<3+4<<endl;
     cout<<8*8;<<endl;
 }
