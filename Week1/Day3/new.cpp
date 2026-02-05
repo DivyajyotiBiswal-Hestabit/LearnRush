9f49351b1882f75b93cd8eb05e467c38ef89ee4f
DivyajyotiBiswal-Hestabit <divyajyotib.hestabit@gmail.com>
2026-02-05 07:49:05 +0530

    commit9

--- Week1/Day3/new.cpp
+++ Week1/Day3/new.cpp
@@ -1,8 +1,12 @@
 #include<iostream>
 using namespace std;
+int sub(int a, int b){
+    return a-b;
+}
 int main(){
     cout<<"Hii world"<<endl;
     cout<<"Hii Hestabit"<<endl;
     cout<"Hiiii"
     cout<<5+6<<endl; 
+    cout<<sub(9,8)<<endl;
 }
