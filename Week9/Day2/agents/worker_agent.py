1150614b6015d17c73d95293cc1150c04dfa3c7f
DivyajyotiBiswal-Hestabit <divyajyotib.hestabit@gmail.com>
2026-04-22 23:57:42 +0530

    remove old week9

--- Week9/Day2/agents/worker_agent.py
+++
@@ -1,8 +0,0 @@
-from utils import call_llm
-from config import WORKER_PROMPT
-
-
-class WorkerAgent:
-    def execute(self, task):
-        messages = [{"role": "user", "content": task}]
-        return call_llm(WORKER_PROMPT, messages)
